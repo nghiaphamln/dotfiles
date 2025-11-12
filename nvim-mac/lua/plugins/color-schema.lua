310a6e2f39965ba873e213740c85f971eba2207b
Minh Nghĩa <nghiaphamln3@gmail.com>
2025-11-12 13:37:47 +0700

    chore(nvim): fix conflict copilot keymap; change color schema

--- nvim-mac/lua/plugins/color-schema.lua
+++ nvim-mac/lua/plugins/color-schema.lua
@@ -1,12 +1,18 @@
 return {
 	"catppuccin/nvim",
-    lazy = false,
+	lazy = false,
 	priority = 1000,
 	config = function()
-        require("catppuccin").setup({
-            variant = "auto",
-            transparent = true,
-        })
+		require("catppuccin").setup({
+			-- variant = "auto",
+			-- transparent = true,
+			flavour = "mocha",
+			background = {
+				light = "latte",
+				dark = "mocha",
+			},
+			transparent_background = false,
+		})
 		vim.cmd([[colorscheme catppuccin]])
 	end,
 }
--- nvim-mac/lua/plugins/copilot.lua
+++ nvim-mac/lua/plugins/copilot.lua
@@ -3,9 +3,9 @@ return {
 	config = function()
 		-- Prevent Copilot from mapping Tab (we use super-tab preset in blink)
 		vim.g.copilot_no_tab_map = true
-		
+
 		-- Map Enter to accept Copilot suggestion
-		vim.keymap.set("i", "<CR>", 'copilot#Accept("\\<CR>")', {
+		vim.keymap.set("i", "<C-CR>", 'copilot#Accept("\\<CR>")', {
 			expr = true,
 			replace_keycodes = false,
 			noremap = true,
