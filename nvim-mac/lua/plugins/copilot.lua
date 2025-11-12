a1f61d853e487a39e8aaaa2cd2e76bbf4d41cfa8
Minh Nghĩa <nghiaphamln3@gmail.com>
2025-11-12 11:26:13 +0700

    feat(plugins): update keymap to use super-tab preset in blink and configure Copilot to prevent Tab mapping

--- nvim-mac/lua/plugins/copilot.lua
+++ nvim-mac/lua/plugins/copilot.lua
@@ -1,3 +1,14 @@
 return {
 	"github/copilot.vim",
+	config = function()
+		-- Prevent Copilot from mapping Tab (we use super-tab preset in blink)
+		vim.g.copilot_no_tab_map = true
+		
+		-- Map Enter to accept Copilot suggestion
+		vim.keymap.set("i", "<CR>", 'copilot#Accept("\\<CR>")', {
+			expr = true,
+			replace_keycodes = false,
+			noremap = true,
+		})
+	end,
 }
