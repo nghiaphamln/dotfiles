99475fabff76bf1dad713943b5bd530189c994a0
Minh Nghĩa <nghiaphamln3@gmail.com>
2025-12-03 13:45:50 +0700

    feat(nvim): add key mapping for Treesitter selection in Flash plugin

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -4,5 +4,18 @@ return {
   opts = {},
   keys = {
     { ";", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    {
+      "<c-space>",
+      mode = { "n", "o", "x" },
+      function()
+        require("flash").treesitter({
+          actions = {
+            ["<c-space>"] = "next",
+            ["<BS>"] = "prev",
+          },
+        })
+      end,
+      desc = "Treesitter Selection",
+    },
   },
 }
