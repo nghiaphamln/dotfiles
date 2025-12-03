115c98a0a0f32df1b320ea13997e55e4855f62e0
Minh Nghĩa <nghiaphamln3@gmail.com>
2025-12-03 13:29:12 +0700

    fix(nvim): correct highlight on yank function call and refactor which-key registration

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -119,22 +119,22 @@ return {
           vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
           vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
           vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-
-          -- Register with which-key
-          local wk = require("which-key")
-          wk.add({
-            { "g", group = "LSP" },
-            { "gd", vim.lsp.buf.definition, desc = "Go to definition" },
-            { "gD", vim.lsp.buf.declaration, desc = "Go to declaration" },
-            { "gi", vim.lsp.buf.implementation, desc = "Go to implementation" },
-            { "gu", vim.lsp.buf.references, desc = "Go to usage/references" },
-            { "K", vim.lsp.buf.hover, desc = "Hover" },
-            { "<leader>rn", vim.lsp.buf.rename, desc = "Rename" },
-            { "<leader>ca", vim.lsp.buf.code_action, desc = "Code action" },
-            { "[d", vim.diagnostic.goto_prev, desc = "Previous diagnostic" },
-            { "]d", vim.diagnostic.goto_next, desc = "Next diagnostic" },
-          }, { buffer = ev.buf })
         end,
+      })
+
+      -- Register with which-key (once, outside LspAttach)
+      local wk = require("which-key")
+      wk.add({
+        { "g", group = "LSP" },
+        { "gd", desc = "Go to definition" },
+        { "gD", desc = "Go to declaration" },
+        { "gi", desc = "Go to implementation" },
+        { "gu", desc = "Go to usage/references" },
+        { "K", desc = "Hover" },
+        { "<leader>rn", desc = "Rename" },
+        { "<leader>ca", desc = "Code action" },
+        { "[d", desc = "Previous diagnostic" },
+        { "]d", desc = "Next diagnostic" },
       })
     end,
   },
